eaea71170b945b04e969e5c87fb98be18369c543
thekcr85 <thekcr85@gmail.com>
2025-12-10 17:01:38 +0100

    feat: Add IAuthorService interface with CRUD and domain-specific methods

---
+++ MiniBlogApp/Services/Interfaces/IAuthorService.cs
@@ -0,0 +1,18 @@
+﻿using MiniBlogApp.Models;
+
+namespace MiniBlogApp.Services.Interfaces;
+
+public interface IAuthorService
+{
+	Task<IEnumerable<Author>> GetAllAsync(CancellationToken cancellationToken = default);
+	Task<Author?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+	Task<Author> CreateAsync(Author author, CancellationToken cancellationToken = default);
+	Task UpdateAsync(Author author, CancellationToken cancellationToken = default);
+	Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+	Task<Author?> GetByIdWithPostsAsync(int id, CancellationToken cancellationToken = default);
+	Task<IEnumerable<Author>> GetAllWithPostsAsync(CancellationToken cancellationToken = default);
+	Task<bool> ExistsAsync(int authorId, CancellationToken cancellationToken = default);
+	Task<Author?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+
+}
