4a648ff78467e49c145289286f2c536016f985ed
thekcr85 <thekcr85@gmail.com>
2025-12-10 16:42:04 +0100

    feat: Declare AuthorRepository methods in IAuthorRepository interface

--- MiniBlogApp/Data/Interfaces/IAuthorRepository.cs
+++ MiniBlogApp/Data/Interfaces/IAuthorRepository.cs
@@ -4,4 +4,9 @@ namespace MiniBlogApp.Data.Interfaces;
 
 public interface IAuthorRepository : IRepositoryBase<Author>
 {
+	Task<Author?> GetAuthorWithPostsAsync(int authorId, CancellationToken cancellationToken = default);
+	Task<IEnumerable<Author>> GetAllAuthorsWithPostsAsync(CancellationToken cancellationToken = default);
+	Task<bool> AuthorExistsAsync(int authorId, CancellationToken cancellationToken = default);
+	Task<Author?> GetAuthorByEmailAsync(string email, CancellationToken cancellationToken = default);
 }
+
