7cc313988237666d8bf0321afef82202cdcd1686
thekcr85 <thekcr85@gmail.com>
2025-12-10 15:42:34 +0100

    refactor: Use primary constructor

--- MiniBlogApp/Data/Repositories/RepositoryBase.cs
+++ MiniBlogApp/Data/Repositories/RepositoryBase.cs
@@ -3,39 +3,30 @@ using MiniBlogApp.Data.Interfaces;
 
 namespace MiniBlogApp.Data.Repositories;
 
-public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+public class RepositoryBase<TEntity>(ApplicationDbContext applicationDbContext, DbSet<TEntity> entities) : IRepositoryBase<TEntity> where TEntity : class
 {
-    private readonly ApplicationDbContext _context;
-    private readonly DbSet<TEntity> _set;
-
-    public RepositoryBase(ApplicationDbContext context)
-    {
-        _context = context;
-        _set = context.Set<TEntity>();
-    }
-
     public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        return await _set.AsNoTracking().ToListAsync(cancellationToken);
+        return await entities.AsNoTracking().ToListAsync(cancellationToken);
     }
 
     public async Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
-        return await _set.FindAsync(new object[] { id }, cancellationToken); 
+        return await entities.FindAsync([id], cancellationToken); 
 	}
 
     public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        await _set.AddAsync(entity, cancellationToken);
+        await entities.AddAsync(entity, cancellationToken);
     }
 
     public void Update(TEntity entity)
     {
-        _set.Update(entity);
+        entities.Update(entity);
     }
 
     public void Remove(TEntity entity)
     {
-        _set.Remove(entity);
+        entities.Remove(entity);
     }
 }
