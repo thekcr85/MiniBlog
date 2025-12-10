3939b5eb47f2955c7c0dcea0a12cfe2950e6ff54
thekcr85 <thekcr85@gmail.com>
2025-12-10 17:05:35 +0100

    feat: Implement GetAll action in AuthorController

---
+++ MiniBlogApp/Controllers/AuthorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniBlogApp.Services.Interfaces;
+
+namespace MiniBlogApp.Controllers;
+
+public class AuthorController(IAuthorService authorService) : Controller
+{
+	public async Task<IActionResult> Index(CancellationToken cancellationToken)
+	{
+		var authors = await authorService.GetAllWithPostsAsync(cancellationToken);
+		return View(authors);
+	}
+}
