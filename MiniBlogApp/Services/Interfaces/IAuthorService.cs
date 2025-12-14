b3c1c8698d0024bd4edf18cecd6da936aed455f7
thekcr85 <thekcr85@gmail.com>
2025-12-14 12:27:14 +0100

    feat: Add new methods for search and get latest 3 authors

--- MiniBlogApp/Services/Interfaces/IAuthorService.cs
+++ MiniBlogApp/Services/Interfaces/IAuthorService.cs
@@ -13,6 +13,6 @@ public interface IAuthorService
 	Task<IEnumerable<Author>> GetAllWithPostsAsync(CancellationToken cancellationToken = default);
 	Task<bool> ExistsAsync(int authorId, CancellationToken cancellationToken = default);
 	Task<Author?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-
-
+	Task<IEnumerable<Author>> SearchAuthorsByNameAsync(string searchTerm, CancellationToken cancellationToken = default);
+	Task<IEnumerable<Author>> GetLatestAuthorsAsync(int count = 3, CancellationToken cancellationToken = default);
 }
