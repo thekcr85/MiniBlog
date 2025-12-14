a20bbc13fa4d385edbd7dc3738c17998beafcb0d
thekcr85 <thekcr85@gmail.com>
2025-12-14 12:31:33 +0100

    feat: Implement 2 new methods for search and get latest 3 authors

--- MiniBlogApp/Data/Repositories/AuthorRepository.cs
+++ MiniBlogApp/Data/Repositories/AuthorRepository.cs
@@ -33,4 +33,23 @@ public class AuthorRepository(ApplicationDbContext applicationDbContext)
 		return await applicationDbContext.Authors
 			.FirstOrDefaultAsync(a => a.Email == email, cancellationToken);
 	}
+
+	public async Task<IEnumerable<Author>> SearchAuthorsByNameAsync(string searchTerm, CancellationToken cancellationToken = default)
+	{
+		return await applicationDbContext.Authors
+			.Where(a => a.FirstName.Contains(searchTerm) || a.LastName.Contains(searchTerm) || a.Email.Contains(searchTerm))
+			.Include(a => a.BlogPosts)
+			.AsNoTracking()
+			.ToListAsync(cancellationToken);
+	}
+
+	public async Task<IEnumerable<Author>> GetLatestAuthorsAsync(int count = 3, CancellationToken cancellationToken = default)
+	{
+		return await applicationDbContext.Authors
+			.OrderByDescending(a => a.CreatedAt)
+			.Take(count)
+			.Include(a => a.BlogPosts)
+			.AsNoTracking()
+			.ToListAsync(cancellationToken);
+	}
 }
