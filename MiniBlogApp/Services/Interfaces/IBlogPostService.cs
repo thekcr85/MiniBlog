bc9d96660de9f8fda19afd8aed56e87c5b6f87bc
thekcr85 <thekcr85@gmail.com>
2025-12-15 14:05:17 +0100

    feat: Add methods from IRepositoryBase and IBlogPostRepository for actions on BlogPost

---
+++ MiniBlogApp/Services/Interfaces/IBlogPostService.cs
@@ -0,0 +1,17 @@
+﻿using MiniBlogApp.Models;
+
+namespace MiniBlogApp.Services.Interfaces;
+
+public interface IBlogPostService
+{
+	Task<IEnumerable<BlogPost>> GetAllAsync(CancellationToken cancellationToken = default);
+
+	Task<BlogPost?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+	Task<BlogPost> CreateAsync(BlogPost blogPost, CancellationToken cancellationToken = default);
+	Task UpdateAsync(BlogPost blogPost, CancellationToken cancellationToken = default);
+	Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+
+	Task<BlogPost?> GetPostWithCommentsAsync(int id, CancellationToken cancellationToken = default);
+	Task<IEnumerable<BlogPost>> GetPostsByAuthor(int authorId, CancellationToken cancellationToken = default);
+	Task<IEnumerable<BlogPost>> GetLatestPostsAsync(int count = 3, CancellationToken cancellationToken = default);
+}
