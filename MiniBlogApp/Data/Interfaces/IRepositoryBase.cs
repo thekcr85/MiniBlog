c257d0de29120c42e867231d64fe4cc74bf26742
thekcr85 <thekcr85@gmail.com>
2025-12-15 16:37:50 +0100

    feat: Change Update/Remove to async pattern in IRepositoryBase

--- MiniBlogApp/Data/Interfaces/IRepositoryBase.cs
+++ MiniBlogApp/Data/Interfaces/IRepositoryBase.cs
@@ -9,6 +9,6 @@ public interface IRepositoryBase<TEntity> where TEntity : class
     Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
     Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
-    void Update(TEntity entity);
-    void Remove(TEntity entity);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
 }
