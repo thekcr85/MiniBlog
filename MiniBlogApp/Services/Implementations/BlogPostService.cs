ea3943a2c17fa89a332916c89776d11d0e90207b
thekcr85 <thekcr85@gmail.com>
2025-12-15 14:18:40 +0100

    feat: Implement 3 methods from interface IBlogPostService

---
+++ MiniBlogApp/Services/Implementations/BlogPostService.cs
@@ -0,0 +1,58 @@
+﻿using MiniBlogApp.Data.Interfaces;
+using MiniBlogApp.Models;
+using MiniBlogApp.Services.Interfaces;
+
+namespace MiniBlogApp.Services.Implementations;
+
+public class BlogPostService(IBlogPostRepository blogPostRepository) : IBlogPostService
+{
+	public async Task<IEnumerable<BlogPost>> GetAllAsync(CancellationToken cancellationToken = default)
+	{
+		return await blogPostRepository.GetAllAsync(cancellationToken);
+	}
+
+	public async Task<BlogPost?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+	{
+		if (id <= 0)
+		{
+			throw new ArgumentException("Invalid blog post ID.", nameof(id));
+		}
+
+		return await blogPostRepository.GetByIdAsync(id, cancellationToken);
+	}
+
+	public async Task<BlogPost> CreateAsync(BlogPost blogPost, CancellationToken cancellationToken = default)
+	{
+		if (blogPost == null)
+		{
+			throw new ArgumentNullException(nameof(blogPost), "Blog post cannot be null.");
+		}
+		await blogPostRepository.AddAsync(blogPost, cancellationToken);
+		return blogPost;
+	}
+
+	public Task UpdateAsync(BlogPost blogPost, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<BlogPost?> GetPostWithCommentsAsync(int id, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<BlogPost>> GetPostsByAuthor(int authorId, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<BlogPost>> GetLatestPostsAsync(int count = 3, CancellationToken cancellationToken = default)
+	{
+		throw new NotImplementedException();
+	}
+}
