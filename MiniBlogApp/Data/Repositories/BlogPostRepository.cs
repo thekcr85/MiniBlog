b3ca1c4a35419d4cd901dc3148b2d379aadee9b0
thekcr85 <thekcr85@gmail.com>
2025-12-15 13:59:47 +0100

    feat: Implement methods from IBlogPostRepository interface

---
+++ MiniBlogApp/Data/Repositories/BlogPostRepository.cs
@@ -0,0 +1,32 @@
+﻿using MiniBlogApp.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using MiniBlogApp.Models;
+
+namespace MiniBlogApp.Data.Repositories;
+
+public class BlogPostRepository(ApplicationDbContext applicationDbContext) : RepositoryBase<BlogPost>(applicationDbContext, applicationDbContext.BlogPosts), IBlogPostRepository
+{
+	public async Task<IEnumerable<BlogPost>> GetLatestPostsAsync(int count = 3, CancellationToken cancellationToken = default)
+	{
+		return await applicationDbContext.BlogPosts
+			.OrderByDescending(bp => bp.CreatedAt)
+			.Take(count) // Take only the specified number of latest posts
+			.AsNoTracking()
+			.ToListAsync(cancellationToken); 
+	}
+
+	public async Task<IEnumerable<BlogPost>> GetPostsByAuthor(int authorId, CancellationToken cancellationToken = default)
+	{
+		return await applicationDbContext.BlogPosts
+			.Where(bp => bp.AuthorId == authorId)
+			.AsNoTracking()
+			.ToListAsync(cancellationToken);
+	}
+
+	public async Task<BlogPost?> GetPostWithCommentsAsync(int id, CancellationToken cancellationToken = default)
+	{
+		return await applicationDbContext.BlogPosts
+			.Include(bp => bp.Comments)
+			.FirstOrDefaultAsync(bp => bp.Id == id, cancellationToken);
+	}
+}
